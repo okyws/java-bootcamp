f061dbc97953f2928a83c73d95df3af91a3f0383
okyws <okywahyu1@gmail.com>
2023-06-30 13:41:58 +0700

    fixing code

--- Pretest/Warnet.java
+++ Pretest/Warnet.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Warnet {
 
-  // Konstanta untuk menyimpan nilai tarif dan harga paket
+  // Konstanta
   public static final int TARIF_REGULER = 5000;
   public static final int TARIF_VIP = 7000;
   public static final int HARGA_PAKET_MALAM_REGULER = 20000;
@@ -11,17 +11,15 @@ public class Warnet {
   public static void main(String[] args) {
     // Deklarasi variabel
     int jam, biaya;
-    int vip; // Mengganti boolean menjadi int
+    int vip;
     boolean member, paketMalam;
 
-    // Membuat objek Scanner untuk input
     Scanner input = new Scanner(System.in);
 
     // Input data
     System.out.println("Selamat datang di warnet Cihampelas!");
-    System.out.println("Apakah Anda menggunakan PC vip? (0 untuk reguler, 1 untuk vip)"); // Mengganti true/false
-                                                                                          // menjadi 0/1
-    vip = input.nextInt(); // Mengganti nextBoolean menjadi nextInt
+    System.out.println("Apakah Anda menggunakan PC vip? (0 untuk reguler, 1 untuk vip)");
+    vip = input.nextInt();
     System.out.println("Apakah Anda menjadi member warnet? (true/false)");
     member = input.nextBoolean();
     System.out.println("Berapa lama Anda bermain? (dalam jam)");
@@ -33,14 +31,11 @@ public class Warnet {
     if (paketMalam) {
       // Jika bermain di dalam paket malam, biaya terdiri dari harga paket ditambah
       // tarif reguler atau vip untuk jam-jam di luar paket
-      // Menggunakan operator ternary untuk menggantikan if-else
-      biaya = vip == 1 ? HARGA_PAKET_MALAM_VIP : HARGA_PAKET_MALAM_REGULER; // Mengganti vip ? ... : ... menjadi vip ==
-                                                                            // 1 ? ... : ...
+      biaya = vip == 1 ? HARGA_PAKET_MALAM_VIP : HARGA_PAKET_MALAM_REGULER;
       // Jika bermain lebih dari 7 jam, maka ada jam-jam di luar paket malam yang
       // harus dibayar dengan tarif reguler atau vip
       if (jam > 7) {
-        biaya += (jam - 7) * (vip == 1 ? TARIF_VIP : TARIF_REGULER); // Mengganti vip ? ... : ... menjadi vip == 1 ? ...
-                                                                     // : ...
+        biaya += (jam - 7) * (vip == 1 ? TARIF_VIP : TARIF_REGULER);
         // Jika menjadi member, ada bonus gratis satu jam setiap kelipatan 3 jam untuk
         // jam-jam di luar paket malam
         if (member) {
@@ -51,10 +46,10 @@ public class Warnet {
       // Jika bermain di luar paket malam, biaya tergantung jam dan jenis PC
       // Menggunakan switch-case untuk menangani kasus-kasus yang berbeda
       switch (vip) {
-        case 1: // Mengganti case true menjadi case 1
+        case 1:
           biaya = jam * TARIF_VIP;
           break;
-        case 0: // Mengganti case false menjadi case 0
+        case 0:
           biaya = jam * TARIF_REGULER;
           break;
         default:
